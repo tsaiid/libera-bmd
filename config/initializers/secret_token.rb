b697540ae5f94e105acbcde3668d2205ff1ca28b
I-Ta Tsai <ittsai@gmail.com>
2013-11-04 20:59:45 +0800

    Init a Rails 4 App.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BoneDensityAnalyzer::Application.config.secret_key_base = '477b038fb7f1c9089f67a2adc862e58e76b88dc7204f33df11d1aeea36cedd30df6c4500d2356b03a1578cef26fb1dd32ce68d2eb490964900b8b3a0de80da88'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+BoneDensityAnalyzer::Application.config.session_store :cookie_store, key: '_bone-density-analyzer_session'
