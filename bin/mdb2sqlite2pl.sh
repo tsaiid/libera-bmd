1c92da457e9b33e45d59d0fc8f1a1b9e1d0922cb
I-Ta Tsai <ittsai@gmail.com>
2013-11-06 20:28:06 +0800

    a script to convert mdb to sql and use plural table names.

---
+++ bin/mdb2sqlite2pl.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+/usr/bin/python mdb2sqlite.py $1 \
+| /usr/bin/sed "s/\`Forearm\`/\`Forearms\`/g" \
+| /usr/bin/sed "s/\`Hip\`/\`Hips\`/g" \
+| /usr/bin/sed "s/\`HipHSA\`/\`HipHSAs\`/g" \
+| /usr/bin/sed "s/\`Lateral\`/\`Laterals\`/g" \
+| /usr/bin/sed "s/\`QcParam\`/\`QcParams\`/g" \
+| /usr/bin/sed "s/\`RefPhysician\`/\`RefPhysicians\`/g" \
+| /usr/bin/sed "s/\`Spine\`/\`Spines\`/g" \
+| /usr/bin/sed "s/\`SubRegionComposition\`/\`SubRegionCompositions\`/g" \
+| /usr/bin/sed "s/\`SystemId\`/\`SystemIds\`/g" \
+| /usr/bin/sed "s/\`TenYearFxRisk\`/\`TenYearFxRisks\`/g" \
+| /usr/bin/sed "s/\`Version\`/\`Versions\`/g" \
+| /usr/bin/sed "s/\`Wbody\`/\`Wbodies\`/g" \
+| /usr/bin/sed "s/\`WbodyComposition\`/\`WbodyCompositions\`/g" \
+| /usr/bin/sed "s/\`PATIENT\`/\`PATIENTS\`/g" \
+| /usr/bin/sed "s/\`SubRegionBone\`/\`SubRegionBones\`/g"
