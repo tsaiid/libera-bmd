249c66042a09aca6c0a486efe6c7d9d4f73a8c13
I-Ta Tsai <ittsai@gmail.com>
2013-11-06 20:29:13 +0800

    associate patient to Hips, HipHsas, Laterals.

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -3,4 +3,7 @@ class Patient < ActiveRecord::Base
 
   has_many :Forearms, foreign_key: "PATIENT_KEY"
   has_many :Spines, foreign_key: "PATIENT_KEY"
+  has_many :Hips, foreign_key: "PATIENT_KEY"
+  has_many :HipHsas, foreign_key: "PATIENT_KEY"
+  has_many :Laterals, foreign_key: "PATIENT_KEY"
 end
