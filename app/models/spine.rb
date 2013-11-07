47415e13de191395ad250f85eb9ba2fecdbed9ae
I-Ta Tsai <ittsai@gmail.com>
2013-11-08 01:42:40 +0800

    add methods to access AREA, BMC, BMD.

--- app/models/spine.rb
+++ app/models/spine.rb
@@ -1,3 +1,15 @@
 class Spine < ActiveRecord::Base
   belongs_to :Patient, foreign_key: "PATIENT_KEY"
+
+  def area
+    read_attribute(:TOT_AREA)
+  end
+
+  def bmc
+    read_attribute(:TOT_BMD)
+  end
+
+  def bmd
+    read_attribute(:TOT_BMD)
+  end
 end
