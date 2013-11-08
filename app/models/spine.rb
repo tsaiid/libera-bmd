89f8b91042cab43f0b97f4bcb2f756d2245913ea
I-Ta Tsai <ittsai@gmail.com>
2013-11-08 13:49:45 +0800

    add virtual methods to read attributes.

--- app/models/spine.rb
+++ app/models/spine.rb
@@ -2,15 +2,63 @@ class Spine < ActiveRecord::Base
   belongs_to :Patient, foreign_key: "PATIENT_KEY"
   belongs_to :ScanAnalysis, foreign_key: "SCANID"
 
-  def area
+  def l1_area
+    read_attribute(:L1_AREA)
+  end
+
+  def l1_bmc
+    read_attribute(:L1_BMD)
+  end
+
+  def l1_bmd
+    read_attribute(:L1_BMD)
+  end
+
+  def l2_area
+    read_attribute(:L2_AREA)
+  end
+
+  def l2_bmc
+    read_attribute(:L2_BMD)
+  end
+
+  def l2_bmd
+    read_attribute(:L2_BMD)
+  end
+
+  def l3_area
+    read_attribute(:L3_AREA)
+  end
+
+  def l3_bmc
+    read_attribute(:L3_BMD)
+  end
+
+  def l3_bmd
+    read_attribute(:L3_BMD)
+  end
+
+  def l4_area
+    read_attribute(:L4_AREA)
+  end
+
+  def l4_bmc
+    read_attribute(:L4_BMD)
+  end
+
+  def l4_bmd
+    read_attribute(:L4_BMD)
+  end
+
+  def tot_area
     read_attribute(:TOT_AREA)
   end
 
-  def bmc
+  def tot_bmc
     read_attribute(:TOT_BMD)
   end
 
-  def bmd
+  def tot_bmd
     read_attribute(:TOT_BMD)
   end
 end
