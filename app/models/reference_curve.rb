bbe83cfca498a76c992f7f019e2a35d4f0913a28
I-Ta Tsai <ittsai@gmail.com>
2013-11-08 21:09:40 +0800

    add method to find upper and lower points.

--- app/models/reference_curve.rb
+++ app/models/reference_curve.rb
@@ -2,4 +2,12 @@ class ReferenceCurve < ActiveRecord::Base
   self.primary_key = "UNIQUE_ID"
 
   has_many :Points, foreign_key: :UNIQUE_ID
+
+  def lower_point(age)
+    self.Points.where("X_VALUE <= " + age.to_s).order(X_VALUE: :desc).first
+  end
+
+  def upper_point(age)
+    self.Points.where("X_VALUE >= " + age.to_s).order(X_VALUE: :asc).first
+  end
 end
