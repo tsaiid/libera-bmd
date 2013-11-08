6b901180fb6a76cfa562754de16b4c45ec8c2c99
I-Ta Tsai <ittsai@gmail.com>
2013-11-08 22:00:57 +0800

    add method to calculate T score.

--- app/models/reference_curve.rb
+++ app/models/reference_curve.rb
@@ -10,4 +10,12 @@ class ReferenceCurve < ActiveRecord::Base
   def upper_point(age)
     self.Points.where("X_VALUE >= " + age.to_s).order(X_VALUE: :asc).first
   end
+
+  def t_score(pt_age, bmd)
+    up = self.upper_point(pt_age)
+    lp = self.lower_point(pt_age)
+    ref_bmd = ((pt_age - lp.age) / (up.age - lp.age)) * (up.bmd - lp.bmd) + lp.bmd
+    ref_std = ((pt_age - lp.age) / (up.age - lp.age)) * (up.std - lp.std) + lp.std
+    (bmd - ref_bmd) / ref_std
+  end
 end
