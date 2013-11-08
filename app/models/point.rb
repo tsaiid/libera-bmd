0df180db21c25f0c0bbe6c32e0416ea0f45d598b
I-Ta Tsai <ittsai@gmail.com>
2013-11-08 20:30:46 +0800

    associate point to ref_curve.

--- app/models/point.rb
+++ app/models/point.rb
@@ -1,2 +1,3 @@
 class Point < ActiveRecord::Base
+  belongs_to :ReferenceCurve, foreign_key: "UNIQUE_ID"
 end
--- app/models/reference_curve.rb
+++ app/models/reference_curve.rb
@@ -1,2 +1,5 @@
 class ReferenceCurve < ActiveRecord::Base
+  self.primary_key = "UNIQUE_ID"
+
+  has_many :Points, foreign_key: :UNIQUE_ID
 end
