bfa50edd668a51efbf7db1ac0e91c9ed9ece6887
I-Ta Tsai <ittsai@gmail.com>
2013-11-09 22:36:30 +0800

    convert uppercase to lowercase for table names.

--- bin/mdb2sqlite2pl.sh
+++ bin/mdb2sqlite2pl.sh
@@ -1,19 +1,20 @@
 #!/bin/sh
 
 /usr/bin/python mdb2sqlite.py $1 \
-| /usr/bin/sed "s/\`Forearm\`/\`Forearms\`/g" \
-| /usr/bin/sed "s/\`Hip\`/\`Hips\`/g" \
-| /usr/bin/sed "s/\`HipHSA\`/\`HipHSAs\`/g" \
-| /usr/bin/sed "s/\`Lateral\`/\`Laterals\`/g" \
-| /usr/bin/sed "s/\`QcParam\`/\`QcParams\`/g" \
-| /usr/bin/sed "s/\`RefPhysician\`/\`RefPhysicians\`/g" \
-| /usr/bin/sed "s/\`Spine\`/\`Spines\`/g" \
-| /usr/bin/sed "s/\`SubRegionComposition\`/\`SubRegionCompositions\`/g" \
-| /usr/bin/sed "s/\`SystemId\`/\`SystemIds\`/g" \
-| /usr/bin/sed "s/\`ScanAnalysis\`/\`Scan_Analyses\`/g" \
-| /usr/bin/sed "s/\`TenYearFxRisk\`/\`TenYearFxRisks\`/g" \
-| /usr/bin/sed "s/\`Version\`/\`Versions\`/g" \
-| /usr/bin/sed "s/\`Wbody\`/\`Wbodies\`/g" \
-| /usr/bin/sed "s/\`WbodyComposition\`/\`WbodyCompositions\`/g" \
-| /usr/bin/sed "s/\`PATIENT\`/\`PATIENTS\`/g" \
-| /usr/bin/sed "s/\`SubRegionBone\`/\`SubRegionBones\`/g"
+| /usr/bin/sed "s/\`Forearm\`/\`forearms\`/g" \
+| /usr/bin/sed "s/\`Hip\`/\`hips\`/g" \
+| /usr/bin/sed "s/\`HipHSA\`/\`hip_hsas\`/g" \
+| /usr/bin/sed "s/\`Lateral\`/\`laterals\`/g" \
+| /usr/bin/sed "s/\`QcParam\`/\`qc_params\`/g" \
+| /usr/bin/sed "s/\`RefPhysician\`/\`ref_physicians\`/g" \
+| /usr/bin/sed "s/\`Spine\`/\`spines\`/g" \
+| /usr/bin/sed "s/\`SubRegionComposition\`/\`sub_region_compositions\`/g" \
+| /usr/bin/sed "s/\`SystemId\`/\`system_ids\`/g" \
+| /usr/bin/sed "s/\`ScanAnalysis\`/\`scan_analyses\`/g" \
+| /usr/bin/sed "s/\`TenYearFxRisk\`/\`ten_year_fx_risks\`/g" \
+| /usr/bin/sed "s/\`Version\`/\`versions\`/g" \
+| /usr/bin/sed "s/\`Wbody\`/\`w_bodies\`/g" \
+| /usr/bin/sed "s/\`WbodyComposition\`/\`w_body_compositions\`/g" \
+| /usr/bin/sed "s/\`PATIENT\`/\`patients\`/g" \
+| /usr/bin/sed "s/\`SubRegionBone\`/\`sub_region_bones\`/g" \
+| /usr/bin/tr "[:upper:]" "[:lower:]"
