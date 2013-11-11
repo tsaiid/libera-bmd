3808d63838cd93a930adfe8da1093e4fe329224f
I-Ta Tsai <ittsai@gmail.com>
2013-11-11 18:51:40 +0800

    can calculate T-score and T-score.

--- app/models/hip.rb
+++ app/models/hip.rb
@@ -4,12 +4,23 @@ class Hip < ActiveRecord::Base
 
   def levels
     array = []
-    array <<= {label: "Neck", area: self.neck_area, bmc: self.neck_bmc, bmd: self.neck_bmd}
-    array <<= {label: "Troch", area: self.troch_area, bmc: self.troch_bmc, bmd: self.troch_bmd}
-    array <<= {label: "Inter", area: self.inter_area, bmc: self.inter_bmc, bmd: self.inter_bmd}
-    array <<= {label: "Total", area: self.total_area, bmc: self.total_bmc, bmd: self.total_bmd}
-    array <<= {label: "Ward's", area: self.wards_area, bmc: self.wards_bmc, bmd: self.wards_bmd}
-  end
+    array <<= {label: "Neck", bone_range: "1...", area: self.neck_area, bmc: self.neck_bmc, bmd: self.neck_bmd}
+    array <<= {label: "Troch", bone_range: ".2..", area: self.troch_area, bmc: self.troch_bmc, bmd: self.troch_bmd}
+    array <<= {label: "Inter", bone_range: "..3.", area: self.inter_area, bmc: self.inter_bmc, bmd: self.inter_bmd}
+    array <<= {label: "Total", bone_range: "123.", area: self.total_area, bmc: self.total_bmc, bmd: self.total_bmd}
+    array <<= {label: "Ward's", bone_range: "...4", area: self.wards_area, bmc: self.wards_bmc, bmd: self.wards_bmd}
+
+    ## calculate Z-score
+    age = self.patient.age
+    array.each do |a|
+      ref_curve = self.scan_analysis.find_reference(a[:bone_range])
+      a[:z_score] = ref_curve.z_score(age, a[:bmd])
+      ## incomplete.
+      ## Z-score percentage.
+      ## T-score
+      a[:t_score] = ref_curve.t_score(a[:bmd])
+      ## T-score percentage.
+    end  end
 
   def neck_area
     read_attribute(:neck_area)
