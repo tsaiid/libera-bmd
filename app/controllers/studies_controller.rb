fc577cc44f2743e8495804d73f7d2be293f8e7a3
I-Ta Tsai <ittsai@gmail.com>
2013-11-15 00:33:01 +0800

    calculate the conclusion in the controller region. due to no model is between patient and scan_analysis.

--- app/controllers/studies_controller.rb
+++ app/controllers/studies_controller.rb
@@ -6,5 +6,30 @@ class StudiesController < ApplicationController
   def show
     @studies = ScanAnalysis.where(accession_no: params[:acc])
     @patient = @studies.first.patient
+    @conclusion = conclusion(@studies)
+  end
+
+  private
+  def conclusion(studies)
+    scores = []
+    studies.each do |study|
+      scores << study.score
+    end
+    case studies.first.t_or_z
+    when 't'
+      if scores.min <= -2.5
+        "The BMD meets the criteria of osteoporosis, according to the WHO (World Health Organization) classification."
+      elsif scores.min < -1.0
+        "The BMD meets the criteria of low bone mass, according to the WHO (World Health Organization) classification."
+      else
+        "The BMD meets the criteria of normal limit, according to the WHO (World Health Organization) classification."
+      end
+    when 'z'
+      if scores.min < 2
+        "The BMD meeting the criteria is within the expected range of age, according to 2007 ISCD (the International Society for Clinical Densitometry) combined official positions."
+      else
+        "The BMD meeting the criteria is below the expected range of age, according to 2007 ISCD (the International Society for Clinical Densitometry) combined official positions."
+      end
+    end
   end
 end
