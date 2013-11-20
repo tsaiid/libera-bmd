1c6faa8c1eec6f07e6c66f08ee2691cb1f7a8e58
I-Ta Tsai <ittsai@gmail.com>
2013-11-20 21:34:07 +0800

    test for getjson in iframe.

--- public/bookmarklet.js
+++ public/bookmarklet.js
@@ -1,6 +1,10 @@
 $(document).ready(function(){
   t1 = $('#i1').contents().find('#t1');
-  t1.val("t1");
+
+  $.getJSON( "/studies/report/64304529").done(function(data){
+    t1.val(data.report);
+  });
+
   t2 = $('#i2').contents().find('#t2');
   t2.val("t2");
 });
