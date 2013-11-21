6ade724faa91deb76e14db35c0dcade0814d29b0
I-Ta Tsai <ittsai@gmail.com>
2013-11-21 12:39:17 +0800

    因為要在 Smartwonder 裡用，會有 cross domain 的問題。
    http://leopard.in.ua/2012/07/08/using-cors-with-rails/

--- public/bookmarklet.js
+++ public/bookmarklet.js
@@ -2,7 +2,11 @@ $(document).ready(function(){
   acc_no = $('iframe[name=frameWork]').contents().find('#tabIframe2').contents().find('input[name=OldAccNo]').val();
   report_area = $('iframe[name=frameWork]').contents().find('#tabIframe2').contents().find('textarea[name=ReportContent]');
 
-  $.getJSON( "/studies/report/" + acc_no + "/text").done(function(data){
+  $.ajax({
+    dataType: "json",
+    url: "//bda.dev/studies/report/" + acc_no + "/text",
+    crossDomain: true
+  }).done(function(data){
     //report_area.val(data.report);
     alert(data.report);
   });
