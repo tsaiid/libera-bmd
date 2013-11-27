21f5de9decc3b3a411ee0df45fa4d5fac0b5941a
I-Ta Tsai <ittsai@gmail.com>
2013-11-27 14:42:36 +0800

    計算各處之 BMD 並顯示。

--- app/controllers/statistics_controller.rb
+++ app/controllers/statistics_controller.rb
@@ -1,7 +1,26 @@
 class StatisticsController < ApplicationController
   def index
     @studies = ScanAnalysis.pcu_acc_list
-    spine_studies = ScanAnalysis.pcu_list.where(ref_type: "S")
-    @spine_tot_bmd = spine_studies.to_a.sum { |s| s.exam.tot_bmd } / spine_studies.length
+
+    # 計算各部位平均的 BMD
+    studies = ScanAnalysis.pcu_list
+    @spine_tot_bmd = @hip_neck_bmd = @forearm_ru13_bmd = 0
+    @spine_tot_count = @hip_neck_count = @forearm_ru13_count = 0
+    studies.each do |s|
+      case s.type
+      when 'S'
+        @spine_tot_bmd += s.exam.tot_bmd
+        @spine_tot_count += 1
+      when 'H'
+        @hip_neck_bmd += s.exam.neck_bmd
+        @hip_neck_count += 1
+      when 'R'
+        @forearm_ru13_bmd += s.exam.ru13_bmd
+        @forearm_ru13_count += 1
+      end
+    end
+    @spine_avg_tot_bmd = @spine_tot_bmd / @spine_tot_count
+    @hip_avg_neck_bmd = @hip_neck_bmd / @hip_neck_count
+    @forearm_avg_ru13_bmd = @forearm_ru13_bmd / @forearm_ru13_count
   end
 end
