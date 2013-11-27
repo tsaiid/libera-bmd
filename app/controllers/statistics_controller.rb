cd9544bb9c3241ac5bf2081968bbad637f85d0e2
I-Ta Tsai <ittsai@gmail.com>
2013-11-27 13:46:26 +0800

    Count total bmd of spine and display.

--- app/controllers/statistics_controller.rb
+++ app/controllers/statistics_controller.rb
@@ -1,5 +1,7 @@
 class StatisticsController < ApplicationController
   def index
     @studies = ScanAnalysis.pcu_acc_list
+    spine_studies = ScanAnalysis.pcu_list.where(ref_type: "S")
+    @spine_tot_bmd = spine_studies.to_a.sum { |s| s.exam.tot_bmd } / spine_studies.length
   end
 end
