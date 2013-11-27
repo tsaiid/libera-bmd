7441d7f6d11ce0e77f74d3a7ee1f6d8d7dca57f2
I-Ta Tsai <ittsai@gmail.com>
2013-11-27 15:01:56 +0800

    計算各部位之標準差，並顯示之。
    以小數點後四位來顯示。

--- app/controllers/statistics_controller.rb
+++ app/controllers/statistics_controller.rb
@@ -19,8 +19,33 @@ class StatisticsController < ApplicationController
         @forearm_ru13_count += 1
       end
     end
-    @spine_avg_tot_bmd = (@spine_tot_bmd / @spine_tot_count).round(4)
-    @hip_avg_neck_bmd = (@hip_neck_bmd / @hip_neck_count).round(4)
-    @forearm_avg_ru13_bmd = (@forearm_ru13_bmd / @forearm_ru13_count).round(4)
+    @spine_avg_tot_bmd = @spine_tot_bmd / @spine_tot_count
+    @hip_avg_neck_bmd = @hip_neck_bmd / @hip_neck_count
+    @forearm_avg_ru13_bmd = @forearm_ru13_bmd / @forearm_ru13_count
+
+    # 計算標準差，採用樣本標準差，自由度為 n-1
+    @spine_tot_std = @hip_neck_std = @forearm_ru13_std = 0
+    @spine_tot_bmd = @hip_neck_bmd = @forearm_ru13_bmd = 0
+    studies.each do |s|
+      case s.type
+      when 'S'
+        @spine_tot_bmd += (s.exam.tot_bmd - @spine_avg_tot_bmd)**2
+      when 'H'
+        @hip_neck_bmd += (s.exam.neck_bmd - @hip_avg_neck_bmd)**2
+      when 'R'
+        @forearm_ru13_bmd += (s.exam.ru13_bmd - @forearm_avg_ru13_bmd)**2
+      end
+    end
+    @spine_tot_std = @spine_tot_bmd / (@spine_tot_count - 1)
+    @hip_neck_std = @hip_neck_bmd / (@hip_neck_count - 1)
+    @forearm_ru13_std = @forearm_ru13_bmd / (@forearm_ru13_count - 1)
+
+    # 用小數點後四位 output
+    @spine_avg_tot_bmd = @spine_avg_tot_bmd.round(4)
+    @hip_avg_neck_bmd = @hip_avg_neck_bmd.round(4)
+    @forearm_avg_ru13_bmd = @forearm_avg_ru13_bmd.round(4)
+    @spine_tot_std = @spine_tot_std.round(4)
+    @hip_neck_std = @hip_neck_std.round(4)
+    @forearm_ru13_std = @forearm_ru13_std.round(4)
   end
 end
