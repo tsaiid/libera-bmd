091a408362ad65a276868be81a4c0456ad21e70a
I-Ta Tsai <ittsai@gmail.com>
2013-11-29 19:30:29 +0800

    一些需要 read_attribute 的好像都是脫褲子放屁？

--- app/models/spine.rb
+++ app/models/spine.rb
@@ -59,66 +59,6 @@ class Spine < ActiveRecord::Base
     str
   end
 
-  def l1_area
-    read_attribute(:l1_area)
-  end
-
-  def l1_bmc
-    read_attribute(:l1_bmc)
-  end
-
-  def l1_bmd
-    read_attribute(:l1_bmd)
-  end
-
-  def l2_area
-    read_attribute(:l2_area)
-  end
-
-  def l2_bmc
-    read_attribute(:l2_bmc)
-  end
-
-  def l2_bmd
-    read_attribute(:l2_bmd)
-  end
-
-  def l3_area
-    read_attribute(:l3_area)
-  end
-
-  def l3_bmc
-    read_attribute(:l3_bmc)
-  end
-
-  def l3_bmd
-    read_attribute(:l3_bmd)
-  end
-
-  def l4_area
-    read_attribute(:l4_area)
-  end
-
-  def l4_bmc
-    read_attribute(:l4_bmc)
-  end
-
-  def l4_bmd
-    read_attribute(:l4_bmd)
-  end
-
-  def tot_area
-    read_attribute(:tot_area)
-  end
-
-  def tot_bmc
-    read_attribute(:tot_bmc)
-  end
-
-  def tot_bmd
-    read_attribute(:tot_bmd)
-  end
-
   def l1_included?
     !read_attribute(:l1_included).zero?
   end
