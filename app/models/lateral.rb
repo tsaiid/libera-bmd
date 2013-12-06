ab9c416606634d346b64d58fd1b29b82a3723cb4
I-Ta Tsai <ittsai@gmail.com>
2013-12-07 01:06:25 +0800

    把從 module 繼承回來的 method 改一改。

--- app/models/lateral.rb
+++ app/models/lateral.rb
@@ -4,8 +4,32 @@ class Lateral < ActiveRecord::Base
 
   include SpineGeneral
 
+  def levels
+    array = []
+    array <<= {label: "L1", bone_range: "1...", bmd: l1_bmd, vbmd: l1_vbmd} if l1_included?
+    array <<= {label: "L2", bone_range: ".2..", bmd: l2_bmd, vbmd: l2_vbmd} if l2_included?
+    array <<= {label: "L3", bone_range: "..3.", bmd: l3_bmd, vbmd: l3_vbmd} if l3_included?
+    array <<= {label: "L4", bone_range: "...4", bmd: l4_bmd, vbmd: l4_vbmd} if l4_included?
+    array <<= {label: "Total", bone_range: bone_range, bmd: ltot_bmd, vbmd: ltot_vbmd}
+
+    ## calculate T- and Z-scores
+    array.each do |a|
+      a = calculate_t_z_scores(a)
+    end
+  end
+
+  def t_score
+    level = calculate_t_z_scores({label: "Total", bone_range: bone_range, bmd: ltot_bmd, vbmd: ltot_vbmd})
+    level[:t_score].round(1)
+  end
+
+  def z_score
+    level = calculate_t_z_scores({label: "Total", bone_range: bone_range, bmd: ltot_bmd, vbmd: ltot_vbmd})
+    level[:z_score].round(1)
+  end
+
   def report_str
-    level = calculate_t_z_scores({label: "Total", bone_range: "1234", area: tot_area, bmc: tot_bmc, bmd: tot_bmd})
+    level = calculate_t_z_scores({label: "Total", bone_range: bone_range, bmd: ltot_bmd, vbmd: ltot_vbmd})
     str = "Average bone mineral density (BMD) of L1 to L4 is #{level[:bmd].round(3)} gm/cm2"
     case scan_analysis.t_or_z
     when 't'
