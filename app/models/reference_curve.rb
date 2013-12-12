25b5bd5a1f1ca0941c94e04f071b6e0fa5ac6c2f
I-Ta Tsai <ittsai@gmail.com>
2013-12-12 08:29:53 +0800

    add methods for polynomial curve fitting. However, not use yet.

--- app/models/reference_curve.rb
+++ app/models/reference_curve.rb
@@ -41,6 +41,26 @@ class ReferenceCurve < ActiveRecord::Base
     points.select(:y_value).map {|p| p.y_value}
   end
 
+  def stds
+    points.select(:std).map {|p| p.std}
+  end
+
+  def regress x, y, degree
+    require 'matrix'
+
+    x_data = x.map { |xi| (0..degree).map { |pow| (xi**pow).to_f } }
+
+    mx = Matrix[*x_data]
+    my = Matrix.column_vector(y)
+
+    ((mx.t * mx).inv * mx.t * my).transpose.to_a[0]
+  end
+
+  def curve_fit polynom, x
+    degree = polynom.size - 1
+    (0..degree).inject(0) {|sum, i| sum + polynom[i] * x**i}
+  end
+
 #  private
 #  temp comment out for debug
   def calculate_ref(pt_age)
@@ -60,6 +80,10 @@ class ReferenceCurve < ActiveRecord::Base
       ref_bmd = ((pt_age - lp.age) / (up.age - lp.age)) * (up.bmd - lp.bmd) + lp.bmd
       ref_std = ((pt_age - lp.age) / (up.age - lp.age)) * (up.std - lp.std) + lp.std
     end
+=begin
+    cf_bmd = curve_fit(regress(x_values, y_values, 1), pt_age)
+    cf_std = curve_fit(regress(x_values, stds, 1), pt_age)
+=end
     {bmd: ref_bmd, std: ref_std}
   end
 end
