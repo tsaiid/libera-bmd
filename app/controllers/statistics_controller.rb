3d98c4c773252de571a6910ae61663081ebe69d6
I-Ta Tsai <ittsai@gmail.com>
2013-12-28 16:45:13 +0800

    use more simple rails style.

--- app/controllers/statistics_controller.rb
+++ app/controllers/statistics_controller.rb
@@ -10,72 +10,72 @@ class StatisticsController < ApplicationController
 
     # 計算各部位平均的 BMD
     ## Spine
-    tmp_bmd = Spine.pcu.select("sum(l1_bmd) as bmd").first.bmd
-    @spine_l1_count = Spine.pcu.select("sum(l1_included) as count").first.count
+    tmp_bmd = Spine.pcu.sum(:l1_bmd)
+    @spine_l1_count = Spine.pcu.sum(:l1_included)
     @spine_avg_l1_bmd = tmp_bmd / @spine_l1_count
-    @spine_l1_std = Math.sqrt(Spine.pcu.select("sum((tot_bmd - #{@spine_avg_l1_bmd})*(tot_bmd - #{@spine_avg_l1_bmd})) as delta_sum").first.delta_sum / (@spine_l1_count - 1))
+    @spine_l1_std = Math.sqrt(Spine.pcu.sum("(tot_bmd - #{@spine_avg_l1_bmd})*(tot_bmd - #{@spine_avg_l1_bmd})") / (@spine_l1_count - 1))
 
-    tmp_bmd = Spine.pcu.select("sum(l2_bmd) as bmd").first.bmd
-    @spine_l2_count = Spine.pcu.select("sum(l2_included) as count").first.count
+    tmp_bmd = Spine.pcu.sum(:l2_bmd)
+    @spine_l2_count = Spine.pcu.sum(:l2_included)
     @spine_avg_l2_bmd = tmp_bmd / @spine_l2_count
-    @spine_l2_std = Math.sqrt(Spine.pcu.select("sum((tot_bmd - #{@spine_avg_l2_bmd})*(tot_bmd - #{@spine_avg_l2_bmd})) as delta_sum").first.delta_sum / (@spine_l2_count - 1))
+    @spine_l2_std = Math.sqrt(Spine.pcu.sum("(tot_bmd - #{@spine_avg_l2_bmd})*(tot_bmd - #{@spine_avg_l2_bmd})") / (@spine_l2_count - 1))
 
-    tmp_bmd = Spine.pcu.select("sum(l3_bmd) as bmd").first.bmd
-    @spine_l3_count = Spine.pcu.select("sum(l3_included) as count").first.count
+    tmp_bmd = Spine.pcu.sum(:l3_bmd)
+    @spine_l3_count = Spine.pcu.sum(:l3_included)
     @spine_avg_l3_bmd = tmp_bmd / @spine_l3_count
-    @spine_l3_std = Math.sqrt(Spine.pcu.select("sum((tot_bmd - #{@spine_avg_l3_bmd})*(tot_bmd - #{@spine_avg_l3_bmd})) as delta_sum").first.delta_sum / (@spine_l3_count - 1))
+    @spine_l3_std = Math.sqrt(Spine.pcu.sum("(tot_bmd - #{@spine_avg_l3_bmd})*(tot_bmd - #{@spine_avg_l3_bmd})") / (@spine_l3_count - 1))
 
-    tmp_bmd = Spine.pcu.select("sum(l4_bmd) as bmd").first.bmd
-    @spine_l4_count = Spine.pcu.select("sum(l4_included) as count").first.count
+    tmp_bmd = Spine.pcu.sum(:l4_bmd)
+    @spine_l4_count = Spine.pcu.sum(:l4_included)
     @spine_avg_l4_bmd = tmp_bmd / @spine_l4_count
-    @spine_l4_std = Math.sqrt(Spine.pcu.select("sum((tot_bmd - #{@spine_avg_l4_bmd})*(tot_bmd - #{@spine_avg_l4_bmd})) as delta_sum").first.delta_sum / (@spine_l4_count - 1))
+    @spine_l4_std = Math.sqrt(Spine.pcu.sum("(tot_bmd - #{@spine_avg_l4_bmd})*(tot_bmd - #{@spine_avg_l4_bmd})") / (@spine_l4_count - 1))
 
-    tmp_bmd = Spine.pcu.select("sum(tot_bmd) as bmd").first.bmd
-    @spine_tot_count = Spine.pcu.select("count(*) as count").first.count
+    tmp_bmd = Spine.pcu.sum(:tot_bmd)
+    @spine_tot_count = Spine.pcu.pluck("count(*)").first
     @spine_avg_tot_bmd = tmp_bmd / @spine_tot_count
-    @spine_tot_std = Math.sqrt(Spine.pcu.select("sum((tot_bmd - #{@spine_avg_tot_bmd})*(tot_bmd - #{@spine_avg_tot_bmd})) as delta_sum").first.delta_sum / (@spine_tot_count - 1))
+    @spine_tot_std = Math.sqrt(Spine.pcu.sum("(tot_bmd - #{@spine_avg_tot_bmd})*(tot_bmd - #{@spine_avg_tot_bmd})") / (@spine_tot_count - 1))
 
     ## Hip
-    @hip_count = Hip.pcu.select("count(*) as count").first.count
+    @hip_count = Hip.pcu.pluck("count(*)").first
 
-    tmp_bmd = Hip.pcu.select("sum(neck_bmd) as bmd").first.bmd
+    tmp_bmd = Hip.pcu.sum(:neck_bmd)
     @hip_avg_neck_bmd = tmp_bmd / @hip_count
-    @hip_neck_std = Math.sqrt(Hip.pcu.select("sum((neck_bmd - #{@hip_avg_neck_bmd})*(neck_bmd - #{@hip_avg_neck_bmd})) as delta_sum").first.delta_sum / (@hip_count - 1))
+    @hip_neck_std = Math.sqrt(Hip.pcu.sum("(neck_bmd - #{@hip_avg_neck_bmd})*(neck_bmd - #{@hip_avg_neck_bmd})") / (@hip_count - 1))
 
-    tmp_bmd = Hip.pcu.select("sum(troch_bmd) as bmd").first.bmd
+    tmp_bmd = Hip.pcu.sum(:troch_bmd)
     @hip_avg_troch_bmd = tmp_bmd / @hip_count
-    @hip_troch_std = Math.sqrt(Hip.pcu.select("sum((troch_bmd - #{@hip_avg_troch_bmd})*(troch_bmd - #{@hip_avg_troch_bmd})) as delta_sum").first.delta_sum / (@hip_count - 1))
+    @hip_troch_std = Math.sqrt(Hip.pcu.sum("(troch_bmd - #{@hip_avg_troch_bmd})*(troch_bmd - #{@hip_avg_troch_bmd})") / (@hip_count - 1))
 
-    tmp_bmd = Hip.pcu.select("sum(inter_bmd) as bmd").first.bmd
+    tmp_bmd = Hip.pcu.sum(:inter_bmd)
     @hip_avg_inter_bmd = tmp_bmd / @hip_count
-    @hip_inter_std = Math.sqrt(Hip.pcu.select("sum((inter_bmd - #{@hip_avg_inter_bmd})*(inter_bmd - #{@hip_avg_inter_bmd})) as delta_sum").first.delta_sum / (@hip_count - 1))
+    @hip_inter_std = Math.sqrt(Hip.pcu.sum("(inter_bmd - #{@hip_avg_inter_bmd})*(inter_bmd - #{@hip_avg_inter_bmd})") / (@hip_count - 1))
 
-    tmp_bmd = Hip.pcu.select("sum(htot_bmd) as bmd").first.bmd
+    tmp_bmd = Hip.pcu.sum(:htot_bmd)
     @hip_avg_htot_bmd = tmp_bmd / @hip_count
-    @hip_htot_std = Math.sqrt(Hip.pcu.select("sum((htot_bmd - #{@hip_avg_htot_bmd})*(htot_bmd - #{@hip_avg_htot_bmd})) as delta_sum").first.delta_sum / (@hip_count - 1))
+    @hip_htot_std = Math.sqrt(Hip.pcu.sum("(htot_bmd - #{@hip_avg_htot_bmd})*(htot_bmd - #{@hip_avg_htot_bmd})") / (@hip_count - 1))
 
-    tmp_bmd = Hip.pcu.select("sum(wards_bmd) as bmd").first.bmd
+    tmp_bmd = Hip.pcu.sum(:wards_bmd)
     @hip_avg_wards_bmd = tmp_bmd / @hip_count
-    @hip_wards_std = Math.sqrt(Hip.pcu.select("sum((wards_bmd - #{@hip_avg_wards_bmd})*(wards_bmd - #{@hip_avg_wards_bmd})) as delta_sum").first.delta_sum / (@hip_count - 1))
+    @hip_wards_std = Math.sqrt(Hip.pcu.sum("(wards_bmd - #{@hip_avg_wards_bmd})*(wards_bmd - #{@hip_avg_wards_bmd})") / (@hip_count - 1))
 
     ## Forearm
-    @forearm_count = Forearm.pcu.select("count(*) as count").first.count
+    @forearm_count = Forearm.pcu.pluck("count(*)").first
 
-    tmp_bmd = Forearm.pcu.select("sum(ru13tot_bmd) as bmd").first.bmd
+    tmp_bmd = Forearm.pcu.sum(:ru13tot_bmd)
     @forearm_avg_ru13_bmd = tmp_bmd / @forearm_count
-    @forearm_ru13_std = Math.sqrt(Forearm.pcu.select("sum((ru13tot_bmd - #{@forearm_avg_ru13_bmd})*(ru13tot_bmd - #{@forearm_avg_ru13_bmd})) as delta_sum").first.delta_sum / (@forearm_count - 1))
+    @forearm_ru13_std = Math.sqrt(Forearm.pcu.sum("(ru13tot_bmd - #{@forearm_avg_ru13_bmd})*(ru13tot_bmd - #{@forearm_avg_ru13_bmd})") / (@forearm_count - 1))
 
-    tmp_bmd = Forearm.pcu.select("sum(rumidtot_bmd) as bmd").first.bmd
+    tmp_bmd = Forearm.pcu.sum(:rumidtot_bmd)
     @forearm_avg_rumid_bmd = tmp_bmd / @forearm_count
-    @forearm_rumid_std = Math.sqrt(Forearm.pcu.select("sum((rumidtot_bmd - #{@forearm_avg_rumid_bmd})*(rumidtot_bmd - #{@forearm_avg_rumid_bmd})) as delta_sum").first.delta_sum / (@forearm_count - 1))
+    @forearm_rumid_std = Math.sqrt(Forearm.pcu.sum("(rumidtot_bmd - #{@forearm_avg_rumid_bmd})*(rumidtot_bmd - #{@forearm_avg_rumid_bmd})") / (@forearm_count - 1))
 
-    tmp_bmd = Forearm.pcu.select("sum(ruudtot_bmd) as bmd").first.bmd
+    tmp_bmd = Forearm.pcu.sum(:ruudtot_bmd)
     @forearm_avg_ruud_bmd = tmp_bmd / @forearm_count
-    @forearm_ruud_std = Math.sqrt(Forearm.pcu.select("sum((ruudtot_bmd - #{@forearm_avg_ruud_bmd})*(ruudtot_bmd - #{@forearm_avg_ruud_bmd})) as delta_sum").first.delta_sum / (@forearm_count - 1))
+    @forearm_ruud_std = Math.sqrt(Forearm.pcu.sum("(ruudtot_bmd - #{@forearm_avg_ruud_bmd})*(ruudtot_bmd - #{@forearm_avg_ruud_bmd})") / (@forearm_count - 1))
 
-    tmp_bmd = Forearm.pcu.select("sum(rutot_bmd) as bmd").first.bmd
+    tmp_bmd = Forearm.pcu.sum(:rutot_bmd)
     @forearm_avg_rutot_bmd = tmp_bmd / @forearm_count
-    @forearm_rutot_std = Math.sqrt(Forearm.pcu.select("sum((rutot_bmd - #{@forearm_avg_rutot_bmd})*(rutot_bmd - #{@forearm_avg_rutot_bmd})) as delta_sum").first.delta_sum / (@forearm_count - 1))
+    @forearm_rutot_std = Math.sqrt(Forearm.pcu.sum("(rutot_bmd - #{@forearm_avg_rutot_bmd})*(rutot_bmd - #{@forearm_avg_rutot_bmd})") / (@forearm_count - 1))
 
     # 用小數點後四位 output
     @spine_avg_tot_bmd = @spine_avg_tot_bmd.round(4)
