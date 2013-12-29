c2f0162ac6d8ada4d862e85ba53b5fd0800dc30e
I-Ta Tsai <ittsai@gmail.com>
2013-12-29 13:40:49 +0800

    change project url. fix cross domain in IE.

--- public/smartwonder_insert_report.js
+++ public/smartwonder_insert_report.js
@@ -2,9 +2,12 @@ $(document).ready(function(){
   acc_no = $('iframe[name=frameWork]').contents().find('#tabIframe2').contents().find('input[name=OldAccNo]').val();
   report_area = $('iframe[name=frameWork]').contents().find('#tabIframe2').contents().find('textarea[name=ReportContent]');
 
+  // for cross domain support in IE.
+  $.support.cors = true;
+
   $.ajax({
     dataType: "json",
-    url: "//bda.dev/studies/report/" + acc_no + "/text",
+    url: "//bone-density.tsaiid.idv.tw/studies/report/" + acc_no + "/text",
     crossDomain: true
   }).done(function(data){
     //report_area.val(data.report);
