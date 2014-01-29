15b9c551a57cd3269fc19dbb8b1121968800f25d
I-Ta Tsai@MBP <ittsai@gmail.com>
2014-01-29 08:40:43 +0800

    load path for core_ext.

--- config/application.rb
+++ config/application.rb
@@ -19,5 +19,7 @@ module BoneDensityAnalyzer
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+
+    config.autoload_paths += Dir[File.join(Rails.root, "lib", "core_ext", "*.rb")].each {|l| require l }
   end
 end
