bd8ecd8b78b5f58fa28406dcb0c7e30c955320f4
I-Ta Tsai <ittsai@gmail.com>
2014-02-11 20:50:25 +0800

    in order to be the same with Hologic report, should correct -0.0 to 0.0.

--- lib/core_ext/float.rb
+++ lib/core_ext/float.rb
@@ -1,5 +1,7 @@
 class Float
   def round_for_report(ndigits=nil)
-    sprintf "%.#{ndigits.to_i}f", self
+    output = sprintf "%.#{ndigits.to_i}f", self
+    # correct -0.0 to 0.0
+    self.round(ndigits) == 0 && output[0] == "-" ? output[1..-1] : output
   end
 end
