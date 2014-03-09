5ad332db6cf0d9797c161dfbc9f442f85eb820ff
I-Ta Tsai@MBP <ittsai@gmail.com>
2014-03-10 01:03:11 +0800

    use  to get the path of utils.

--- bin/data2sql2pl.sh
+++ bin/data2sql2pl.sh
@@ -1,22 +1,26 @@
 #!/bin/sh
 
-/usr/bin/python mdb2sqlite.py $1 \
-| /usr/bin/sed "s/\`Forearm\`/\`forearms\`/g" \
-| /usr/bin/sed "s/\`Hip\`/\`hips\`/g" \
-| /usr/bin/sed "s/\`HipHSA\`/\`hip_hsas\`/g" \
-| /usr/bin/sed "s/\`Lateral\`/\`laterals\`/g" \
-| /usr/bin/sed "s/\`QcParam\`/\`qc_params\`/g" \
-| /usr/bin/sed "s/\`RefPhysician\`/\`ref_physicians\`/g" \
-| /usr/bin/sed "s/\`Spine\`/\`spines\`/g" \
-| /usr/bin/sed "s/\`SubRegionComposition\`/\`sub_region_compositions\`/g" \
-| /usr/bin/sed "s/\`SystemId\`/\`system_ids\`/g" \
-| /usr/bin/sed "s/\`ScanAnalysis\`/\`scan_analyses\`/g" \
-| /usr/bin/sed "s/\`TenYearFxRisk\`/\`ten_year_fx_risks\`/g" \
-| /usr/bin/sed "s/\`Version\`/\`versions\`/g" \
-| /usr/bin/sed "s/\`Wbody\`/\`w_bodies\`/g" \
-| /usr/bin/sed "s/\`WbodyComposition\`/\`w_body_compositions\`/g" \
-| /usr/bin/sed "s/\`PATIENT\`/\`patients\`/g" \
-| /usr/bin/sed "s/\`SubRegionBone\`/\`sub_region_bones\`/g" \
-| /usr/bin/sed "s/^INSERT/INSERT OR REPLACE/g" \
-| /usr/bin/perl -pe "s/\`([A-Z_a-z0-9]+)\`/\`\L\1\`/g" \
-| /usr/bin/perl -pe "s/\`([A-Z_a-z0-9]+)&([A-Z_a-z0-9]+)\`/\`\L\1_\L\2\`/g"
+PYTHON_PATH=`which python`
+SED_PATH=`which sed`
+PERL_PATH=`which perl`
+
+$PYTHON_PATH mdb2sqlite.py $1 \
+| $SED_PATH "s/\`Forearm\`/\`forearms\`/g" \
+| $SED_PATH "s/\`Hip\`/\`hips\`/g" \
+| $SED_PATH "s/\`HipHSA\`/\`hip_hsas\`/g" \
+| $SED_PATH "s/\`Lateral\`/\`laterals\`/g" \
+| $SED_PATH "s/\`QcParam\`/\`qc_params\`/g" \
+| $SED_PATH "s/\`RefPhysician\`/\`ref_physicians\`/g" \
+| $SED_PATH "s/\`Spine\`/\`spines\`/g" \
+| $SED_PATH "s/\`SubRegionComposition\`/\`sub_region_compositions\`/g" \
+| $SED_PATH "s/\`SystemId\`/\`system_ids\`/g" \
+| $SED_PATH "s/\`ScanAnalysis\`/\`scan_analyses\`/g" \
+| $SED_PATH "s/\`TenYearFxRisk\`/\`ten_year_fx_risks\`/g" \
+| $SED_PATH "s/\`Version\`/\`versions\`/g" \
+| $SED_PATH "s/\`Wbody\`/\`w_bodies\`/g" \
+| $SED_PATH "s/\`WbodyComposition\`/\`w_body_compositions\`/g" \
+| $SED_PATH "s/\`PATIENT\`/\`patients\`/g" \
+| $SED_PATH "s/\`SubRegionBone\`/\`sub_region_bones\`/g" \
+| $SED_PATH "s/^INSERT/INSERT OR REPLACE/g" \
+| $PERL_PATH -pe "s/\`([A-Z_a-z0-9]+)\`/\`\L\1\`/g" \
+| $PERL_PATH -pe "s/\`([A-Z_a-z0-9]+)&([A-Z_a-z0-9]+)\`/\`\L\1_\L\2\`/g"
