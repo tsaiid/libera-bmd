11e9d97f44f146df4448a37290004606c40f0780
I-Ta Tsai@MBP <ittsai@gmail.com>
2014-04-12 17:05:35 +0800

    set relative_url_root for sub-URI deployment.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,6 @@ BoneDensityAnalyzer::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  config.relative_url_root = "/bone-density"
 end
